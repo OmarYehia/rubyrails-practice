8d2a7ff245ff3f646784155df8652ecb02f70109
Omar Yehia <oyehia94@gmail.com>
2021-05-10 22:54:59 +0200

    Serializer added to format the returned response in API

--- myapp/app/controllers/api/v1/articles_controller.rb
+++ myapp/app/controllers/api/v1/articles_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::ArticlesController < ApplicationController
     def index
         @user = User.find(params[:user_id])
         @articles = @user.articles
-        byebug
-        render json: @articles
+        render json: @articles, each_serializer: ArticleSerializer
     end
 end
--- myapp/app/models/user.rb
+++ myapp/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :articles
+  include ActiveModel::SerializerSupport
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   # devise :database_authenticatable, :registerable,
