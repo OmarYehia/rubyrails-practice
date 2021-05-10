586c5ff4345fd9fe55930cfc3f0bf5b062a507bc
Omar Yehia <oyehia94@gmail.com>
2021-05-10 20:07:35 +0200

    Added the permissions for the user to create new posts

--- myapp/app/models/ability.rb
+++ myapp/app/models/ability.rb
@@ -6,6 +6,7 @@ class Ability
   def initialize(user)
     if user.present?  # additional permissions for logged in users (they can read their own posts)
       can :read, Article, user_id: user.id
+      can :create, Article
     end
   end
 end
